6292a9601503029abf29e5084306c51737b1f33b
AlexCopin <alex.copin@outlook.com>
2024-03-16 23:53:03 +0100

    Fix camera & spawn golem with initial MoveTO

--- GargoyleCraft/Source/GargoyleCraft/Golems/Golem.cpp
+++ GargoyleCraft/Source/GargoyleCraft/Golems/Golem.cpp
@@ -1,7 +1,13 @@
 #include "Golem.h"
 
+#include "ActorTransactionAnnotation.h"
+#include "Components/CapsuleComponent.h"
+
 AGolem::AGolem()
 {
+  Root = CreateDefaultSubobject<USceneComponent>("Root");
+  SetRootComponent(Root);
+  GetCapsuleComponent()->SetupAttachment(Root);
   PoolComponent = CreateDefaultSubobject<UC_Pool>("PoolComponent");
   AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("GolemASC");
 }
--- GargoyleCraft/Source/GargoyleCraft/Golems/Golem.h
+++ GargoyleCraft/Source/GargoyleCraft/Golems/Golem.h
@@ -2,16 +2,18 @@
 
 #include "CoreMinimal.h"
 #include "AbilitySystemComponent.h"
+#include "GameFramework/Character.h"
 #include "GargoyleCraft/Components/Golem/C_Pool.h"
 #include "Golem.generated.h"
 
 UCLASS()
-class AGolem : public APawn
+class AGolem : public ACharacter
 {
     GENERATED_BODY()
 public:
   AGolem();
-
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  TObjectPtr<USceneComponent> Root;
   UPROPERTY(EditAnywhere, BlueprintReadWrite)
     TObjectPtr<UC_Pool> PoolComponent;
     UPROPERTY(EditAnywhere, BlueprintReadWrite)
