f966e9d5c99967ac988c28557c68309ebe26a48e
AlexCopin <alex.copin@outlook.com>
2024-08-07 19:04:04 +0200

    Begin abilities & Prepare for FX

---
+++ GargoyleCraft/Source/GargoyleCraft/Golems/GameplayAbilities/GA_BasicMelee.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GargoyleCraft/GameplayAbilitySystem/GameplayAbilities/GC_GameplayAbility_Character.h"
+#include "GA_BasicMelee.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GARGOYLECRAFT_API UGA_BasicMelee : public UGC_GameplayAbility_Character
+{
+	GENERATED_BODY()
+	
+};
